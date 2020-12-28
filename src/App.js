e0fa246c2563f357224d6c2a76b92b8307baac8c
shownola <swasieko@gmail.com>
2020-12-28 16:07:20 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TodoList from './TodoList/TodoList';
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant="h1">Todo Stream</Typography>
+        <hr />
+
+        <TodoList />
       </header>
     </div>
   );
