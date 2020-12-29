8305b8bf74a4a25c8d76c951675571ee7338f276
shownola <swasieko@gmail.com>
2020-12-28 17:57:39 -0700

    Add todoForm and handle submit

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import TodoForm from './TodoForm';
+import TodoItem from './TodoItem';
 
 const api_url = `http://localhost:3001/api/v1/todos`
 
@@ -9,15 +10,43 @@ class TodoList extends Component {
     this.state = {
       items: []
     }
+    this.updateTodoList =this.updateTodoList.bind(this);
+  }
+  componentDidMount(){
+    this.getTasks();
+
   }
 
+  getTasks(){
+    fetch(api_url)
+    .then(response => response.json())
+    .then(response_items => {
+      this.setState({
+        items: response_items
+      })
+    });
+  }
+
+  updateTodoList(item){
+    let _items = this.state.items
+    _items.unshift(item) //Adds to the beginning of array
+    this.setState({
+      items: _items
+    })
+  }
+
+
+
   render (){
+    console.log(this.state.items);
     return(
       <div>
-        <TodoForm />
-        <ul>
-          <li>Todo #1</li>
-          <li>Todo #2</li>
+        <TodoForm api_url={api_url} updateTodoList={this.updateTodoList} />
+        <ul id="todo_list">
+          {this.state.items.map((item) => (
+            <TodoItem key={item.id} item={item} />
+
+          ))}
         </ul>
       </div>
     )
